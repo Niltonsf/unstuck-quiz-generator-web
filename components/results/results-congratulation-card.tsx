401d5daa5e4b678a670703fc48b4915266d7f5fa
Niltonsf <nilton.schumacher.filho@gmail.com>
2025-05-21 17:02:13 -0300

    feat: adds custom message according to score

--- components/results/results-congratulation-card.tsx
+++ components/results/results-congratulation-card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import CorrectAnswer from '@/assets/svg/correct-answer.svg'
 import PartySymbols from '@/assets/svg/results/party-symbols.svg'
 import Image from 'next/image'
@@ -10,7 +10,40 @@ interface ResultsCongratulationCardProps {
   totalCorrectAnswers: number
 }
 
-const ResultsCongratulationCard = ({ totalCorrectAnswers }: ResultsCongratulationCardProps) => {
+const ResultsCongratulationCard = ({
+  totalCorrectAnswers,
+}: ResultsCongratulationCardProps) => {
+  const [name, setName] = useState('')
+
+  const shareToLinkedIn = () => {
+    const shareText = `I scored ${totalCorrectAnswers}/10 on this quiz generator! 🎉 Try it yourself https://niltonsf.dev/`
+    const linkedInUrl = `https://www.linkedin.com/feed/?shareActive=true&text=${shareText}`
+
+    window.open(linkedInUrl, '_blank', 'noopener,noreferrer')
+  }
+
+  const messageByScore = useMemo(() => {
+    if (totalCorrectAnswers === 10) {
+      return `Outstanding, ${name}! You aced the quiz with a perfect score!`
+    } else if (totalCorrectAnswers >= 8) {
+      return `Great job, ${name}! You really know your stuff.`
+    } else if (totalCorrectAnswers >= 5) {
+      return `Well done, ${name}! You’re on the right track. Keep it up!`
+    } else if (totalCorrectAnswers >= 3) {
+      return `Good effort, ${name}. A bit more practice and you’ll get there!`
+    } else {
+      return `Don't give up, ${name}. Every expert was once a beginner.`
+    }
+  }, [name, totalCorrectAnswers])
+
+  useEffect(() => {
+    const savedName = localStorage.getItem('@unstuckquiz:name')
+
+    if (savedName) {
+      setName(savedName || 'John Doe')
+    }
+  }, [])
+
   return (
     <div className="flex flex-col items-center py-9 rounded-md border relative">
       <Image src={PartySymbols} alt={'party-symbols'} fill className="z-[-1]" />
@@ -24,15 +57,21 @@ const ResultsCongratulationCard = ({ totalCorrectAnswers }: ResultsCongratulatio
 
       <div className="flex flex-col items-center mx-5">
         <span className="text-center mt-6 text-2xl font-medium max-w-[450px]">
-          Great Work Martinelli, you did very good on your quiz.
+          {messageByScore}
         </span>
 
-        <span className="text-center mt-9 text-[56px] font-semibold">{totalCorrectAnswers}/10</span>
+        <span className="text-center mt-9 text-[56px] font-semibold">
+          {totalCorrectAnswers}/10
+        </span>
 
-        <DualProgressBar correct={totalCorrectAnswers} total={10} className="mt-5" />
+        <DualProgressBar
+          correct={totalCorrectAnswers}
+          total={10}
+          className="mt-5"
+        />
       </div>
 
-      <Button className="h-10 w-40 mt-7">
+      <Button className="h-10 w-40 mt-7" onClick={shareToLinkedIn}>
         Share results
         <Share />
       </Button>
